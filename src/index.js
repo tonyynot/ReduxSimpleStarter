bae433edae2ae23908ba91800dcb9b1f876d47e3
aramella <aramella@americanbible.org>
2018-04-10 18:00:01 -0400

    handling for null props

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
 import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyCPQc8wgwc2_ekTWMwCmDABO-HIRbYGHoc';
@@ -21,6 +22,7 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
+				<VideoDetail video={this.state.videos[0]} />
 				<VideoList videos={this.state.videos} />
 			</div>
 		);
